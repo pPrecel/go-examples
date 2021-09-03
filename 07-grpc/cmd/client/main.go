5b946a53b74ba5fa122b96f360c36d6146a424a5
pPrecel <filip.strozik@outlook.com>
2021-09-03 11:24:13 +0200

    Implement missing gRPC methods

--- 07-grpc/cmd/client/main.go
+++ 07-grpc/cmd/client/main.go
@@ -2,24 +2,115 @@ package main
 
 import (
 	"context"
+	"io"
+	"time"
 
 	pb "github.com/pPrecel/go-examples/07-grpc/internal/grpc"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
 
-func getValue(c pb.HelloServiceClient) {
-	val := &pb.Value{
-		Val:  4,
-		Name: "hello test name",
-	}
-	logrus.Infof("sending request: {Val: %d, Name: \"%s\"}", val.Val, val.Name)
-	val, err := c.GetValue(context.Background(), val)
+// getValues gets values from the server via stream
+func getValues(c pb.HelloServiceClient, val *pb.Value) {
+	logrus.Info("sending request: {Val: %d, Name: \"%s\"}", val.Val, val.Name)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	svc, err := c.ValueToStream(ctx, val)
 	if err != nil {
 		logrus.Fatalf("can't get value from the server: %v", err)
 	}
 
-	logrus.Infof("received value: {Val: %d, Name: \"%s\"}", val.Val, val.Name)
+	for {
+		res, err := svc.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			logrus.Fatalf("streaming error: %v", err)
+		}
+
+		logrus.Infof("received value from the stream: {Val: %d, Name: \"%s\"}", res.Val, res.Name)
+	}
+
+	logrus.Info("ending connection")
+}
+
+// getValue gets value from the server
+func getValue(c pb.HelloServiceClient, val *pb.Value) {
+	logrus.Infof("sending request: {Val: %d, Name: \"%s\"}", val.Val, val.Name)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	res, err := c.GetValue(ctx, val)
+	if err != nil {
+		logrus.Fatalf("can't get value from the server: %v", err)
+	}
+	logrus.Infof("received value: {Val: %d, Name: \"%s\"}", res.Val, res.Name)
+}
+
+// streamValues sends a few values and then receive and log one value
+func streamValues(c pb.HelloServiceClient, val *pb.Value) {
+	logrus.Infof("sending 5x request: {Val: %d, Name: \"%s\"}", val.Val, val.Name)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	svc, err := c.StreamToValue(ctx)
+	if err != nil {
+		logrus.Fatalf("can't get value from the server: %v", err)
+	}
+
+	for i := 0; i < 5; i++ {
+		err := svc.Send(val)
+		if err != nil {
+			logrus.Fatalf("streaming error: %v", err)
+		}
+		logrus.Infof("value sent: {Val: %d, Name: \"%s\"}", val.Val, val.Name)
+	}
+
+	res, err := svc.CloseAndRecv()
+	if err != nil {
+		logrus.Fatalf("can't get value from the server: %v", err)
+	}
+	logrus.Infof("received value: {Val: %d, Name: \"%s\"}", res.Val, res.Name)
+}
+
+// chat chats with the server :)
+func chat(c pb.HelloServiceClient, val *pb.Value) {
+	logrus.Infof("receiving and sending 5x request: {Val: %d, Name: \"%s\"}", val.Val, val.Name)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	svc, err := c.StreamToStream(ctx)
+	if err != nil {
+		logrus.Fatalf("can't get value from the server: %v", err)
+	}
+
+	for i := 0; i < 5; i++ {
+		err := svc.Send(val)
+		if err != nil {
+			logrus.Fatalf("streaming error: %v", err)
+		}
+		logrus.Infof("value sent: {Val: %d, Name: \"%s\"}", val.Val, val.Name)
+
+		res, err := svc.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			logrus.Fatalf("streaming error: %v", err)
+		}
+		logrus.Infof("received value from the stream: {Val: %d, Name: \"%s\"}", res.Val, res.Name)
+	}
+
+	err = svc.CloseSend()
+	if err != nil {
+		logrus.Fatalf("can't get value from the server: %v", err)
+	}
+
+	logrus.Info("chat ended")
 }
 
 func main() {
@@ -31,5 +122,11 @@ func main() {
 
 	client := pb.NewHelloServiceClient(conn)
 
-	getValue(client)
+	getValue(client, &pb.Value{Val: 4, Name: "hello test name"})
+
+	// getValues(client, &pb.Value{Val: 7, Name: "hello another test"})
+
+	// streamValues(client, &pb.Value{Val: 10, Name: "hello another another test"})
+
+	// chat(client, &pb.Value{Val: 20, Name: "hello chatting machine"})
 }
--- 07-grpc/cmd/server/main.go
+++ 07-grpc/cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io"
 	"net"
 
 	pb "github.com/pPrecel/go-examples/07-grpc/internal/grpc"
@@ -17,6 +18,75 @@ func (s *service) GetValue(c context.Context, v *pb.Value) (*pb.Value, error) {
 	logrus.Infof("received: {Val: %d, Name: \"%s\"}", v.Val, v.Name)
 	logrus.Infof("sending back: {Val: %d, Name: \"%s\"}", v.Val, v.Name)
 	return v, nil
+}
+
+func (s *service) ValueToStream(v *pb.Value, service pb.HelloService_ValueToStreamServer) error {
+	logrus.Infof("received: {Val: %d, Name: \"%s\"}", v.Val, v.Name)
+	for i := 0; i < 5; i++ {
+		err := service.Send(v)
+		if err != nil {
+			logrus.Fatalf("failed to send: {Val: %d, Name: \"%s\"}", v.Val, v.Name)
+		}
+	}
+
+	logrus.Info("all values sent")
+	return nil
+}
+
+func (s *service) StreamToValue(svc pb.HelloService_StreamToValueServer) error {
+	logrus.Info("receving values from the stream")
+
+	for {
+		res, err := svc.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			logrus.Errorf("streaming error: %v", err)
+			return err
+		}
+		logrus.Infof("received value from the stream: {Val: %d, Name: \"%s\"}", res.Val, res.Name)
+	}
+
+	val := &pb.Value{
+		Val:  66,
+		Name: "calling back",
+	}
+	err := svc.SendAndClose(val)
+	if err != nil {
+		logrus.Fatalf("failed to send: {Val: %d, Name: \"%s\"}", val.Val, val.Name)
+	}
+	logrus.Infof("sending back: {Val: %d, Name: \"%s\"}", val.Val, val.Name)
+
+	logrus.Info("ending connection")
+	return nil
+}
+
+func (s *service) StreamToStream(svc pb.HelloService_StreamToStreamServer) error {
+	logrus.Info("receving and sending values from the stream")
+
+	for {
+		res, err := svc.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			logrus.Errorf("streaming error: %v", err)
+			return err
+		}
+		logrus.Infof("received value from the stream: {Val: %d, Name: \"%s\"}", res.Val, res.Name)
+
+		err = svc.Send(res)
+		if err != nil {
+			logrus.Fatalf("failed to send: {Val: %d, Name: \"%s\"}", res.Val, res.Name)
+		}
+		logrus.Infof("sent value to the stream: {Val: %d, Name: \"%s\"}", res.Val, res.Name)
+	}
+
+	logrus.Info("ending connection")
+	return nil
 }
 
 func main() {
